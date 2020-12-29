fa4d7a040bd03e4242fe938f8370ae19f5551a3c
Eric <me@bostrot.com>
2020-12-29 17:03:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const api = new GhostAdminAPI({
   const page = await browser.newPage();
   const VIEWPORT = {    
     width: 1200,
-    height: 100,
+    height: 800,
     deviceScaleFactor: 1,
   };
   await page.setViewport(VIEWPORT);
