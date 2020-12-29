c05c12d445be0ba023bb5ef3295e5785a47886f2
Eric <me@bostrot.com>
2020-12-29 15:49:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ const api = new GhostAdminAPI({
     deviceScaleFactor: 1,
   };
   await page.setViewport(VIEWPORT);
-  await page.goto('https://www.who.int/redirect-pages/page/novel-coronavirus-(covid-19)-situation-dashboard'/*, {
-    waitUntil: ['load', 'networkidle0', 'networkidle2'],
-  }*/);
+  await page.goto('https://www.who.int/redirect-pages/page/novel-coronavirus-(covid-19)-situation-dashboard', {
+    waitUntil: ['load', /*'networkidle0',*/ 'networkidle2'],
+  });
 
   await page.screenshot({
     path: 'map.png',
